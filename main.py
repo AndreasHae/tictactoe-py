a500c38114cacfd7d7769af3a8eff34bc6a79c71
Andreas Haessler <andreas.haessler@outlook.at>
2018-03-18 21:36:55 +0100

    Introduce MVC pattern

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 
-from game import Game
-from components import Field
+from model.game import Game
 
-class TicTacToeWindow(Gtk.Window):
+class View(Gtk.Window):
+
+    __gsignals__ = {
+        'button-clicked': (GObject.SIGNAL_RUN_FIRST, None, (int, int,))
+    }
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="Tic-Tac-Toe")
+        Gtk.Window.__init__(self, title="Tic Tac Toe")
 
-        self.game = Game()
+        self.grid = Gtk.Grid()
+        self.add(self.grid)
 
-        self.button = Gtk.Button(label=self.game.next_player)
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
+        self.btns = [[self.__generate_btn(row, col) for col in range(3)] for row in range(3)]
 
-    def on_button_clicked(self, widget):
-        self.game.turn(0, 0)
-        widget.set_label(self.game.__next_player)
+    def __generate_btn(self, row, col):
+        btn = Gtk.Button()
+        btn.set_size_request(50, 50)
+        btn.connect('clicked', self.on_btn_clicked)
+        self.grid.attach(btn, col, row, 1, 1)
 
+    def on_btn_clicked(self, btn):
+        row = self.grid.child_get_property(btn, 'top-attach')
+        col = self.grid.child_get_property(btn, 'left-attach')
 
+        self.emit('button-clicked', row, col)
 
-#window = TicTacToeWindow()
-window = Gtk.Window(title='Test', resizable=False)
-window.set_default_size(200, 200)
-#window.add(Field())
-window.connect("destroy", Gtk.main_quit)
-window.show_all()
-Gtk.main()
+class Controller:
+
+    def __init__(self, game, view):
+        self.__game = game
+        self.__view = view
+
+        self.__view.connect('button-clicked', self.on_btn_clicked)
+        self.__view.connect('destroy', Gtk.main_quit)
+
+        self.__view.show_all()
+
+    def on_btn_clicked(self, view, row, col):
+        print("{}, {}".format(row, col))
+
+if __name__ == "__main__":
+    Controller(Game(), View())
+    Gtk.main()
